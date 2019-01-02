45483287f61ec50353e10dba3fceaad1d71befd6
cristin <cristin.herrera.guerra@gmail.com>
2019-01-02 15:13:49 -0500

    gastos por usuario

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,6 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
   def index
-     @expenses = Expense.order("date DESC")
+    @expenses = Expense.where(user_id: current_user.id).order("date DESC")
   end
 end
